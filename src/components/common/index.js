996c62aad50b07675971f6989ae7a596e34cd9b5
Aditya Pranav <adityapranav014@gmail.com>
2026-02-08 22:23:48 +0530

    removed unused code

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -2,7 +2,5 @@
  * Barrel export for common components
  */
 
-export { default as ProductCard } from './ProductCard';
-export { default as ProductGrid } from './ProductGrid';
 export { default as ContactInfo } from './ContactInfo';
 export { default as ContactForm } from './ContactForm';
--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -6,4 +6,4 @@ export { default as Logo } from './Logo';
 export { default as Button } from './Button';
 export { default as Input } from './Input';
 export { default as Card } from './Card';
-export { default as Badge } from './Badge';
+
